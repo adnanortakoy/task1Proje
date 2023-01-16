1c1c3aecd05f6857c9969c4287c37348cd72e9e1
muharremfevzi <ortakoyadnan@gmail.com>
2023-01-16 14:24:12 +0300

    ilk version

---
+++ src/Task1Proje.java
@@ -0,0 +1,5 @@
+public class Task1Proje {
+    public static void main(String[] args) {
+        System.out.println("bu ilk versiyondur");
+    }
+}
---
+++ src/tesk1Test.java
@@ -0,0 +1,5 @@
+public class tesk1Test {
+    public static void main(String[] args) {
+        System.out.println("bu bir test projesidir");
+    }
+}
